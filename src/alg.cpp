dca610a091fcdba8d4e8e118f00d53804183b003
OlegOgnevIST3 <124896310+OlegOgnevIST3@users.noreply.github.com>
2023-03-01 22:28:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,53 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+    if (value <= 1) 
+    {
+      return false;
+    }
+    for (int i = 2; i * i <= value; i++)
+    {
+        if (value % i == 0)
+        {
+          return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+    int count = 1;
+    uint64_t s = 2;
+    for (int i = 3; count != n; i++)
+    {
+        if (checkPrime(i)) 
+        {
+            s = i;
+            count++;
+        }
+    }
+    return s;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+    uint64_t i;
+    for (i = value + 1; !checkPrime(i); i++) 
+    {
+    }
+    return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+    uint64_t sum = 0;
+    for (int i = 2; i < hbound; i++)
+    {
+        if (checkPrime(i))
+        {
+          sum += i;
+        }
+    }
+    return sum;
 }
